79b28858bfba1296cde9e564c3567702d0ed5e43
toby <toby@aetherbird.app>
2024-02-19 05:44:20 -0500

    removal of redundant echo in config script

--- .scripts/install-config.sh
+++ .scripts/install-config.sh
@@ -11,15 +11,25 @@ apt-get install -y rsync vim
 # remove any neovim from package manager
 apt-get remove neovim
 
+echo startdir=$(pwd)
 startdir=$(pwd)
+echo mkdir ~/.tmp_neovim
 mkdir ~/.tmp_neovim
+echo cd ~/.tmp_neovim/
 cd ~/.tmp_neovim/
+echo wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+echo rm -rf /opt/nvim
 rm -rf /opt/nvim
-tar xfvz nvim-linux64.tar.gz 
+echo tar xfvz nvim-linux64.tar.gz
+tar xfvz nvim-linux64.tar.gz
+echo mkdir -p /opt/nvim/
 mkdir -p /opt/nvim/
+echo mv ./nvim-linux64/* /opt/nvim/*
 mv ./nvim-linux64/* /opt/nvim/*
+echo cd ~
 cd ~
+echo rm -r ~/.tmp_neovim/
 rm -r ~/.tmp_neovim/
+echo cd $startdir
 cd $startdir
-echo 'export PATH="$PATH:/opt/nvim/bin"' >> ~/.bashrc /root/.bashrc
