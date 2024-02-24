fbca93619c3d6882140d0e289b7560a4e6d82f03
toby <toby@aetherbird.app>
2024-02-23 19:13:56 -0500

    installing nvim pckr

---
+++ .config/nvim/lua/aetherbird/pckr.lua
@@ -0,0 +1,23 @@
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+  if not vim.loop.fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
+  end
+
+  vim.opt.rtp:prepend(pckr_path)
+end
+
+bootstrap_pckr()
+
+require('pckr').add{
+  -- My plugins here
+  -- 'foo1/bar1.nvim';
+  -- 'foo2/bar2.nvim';
+}
